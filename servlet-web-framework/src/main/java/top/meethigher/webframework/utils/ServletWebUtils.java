635824d1f9ee3bc6f5713004a0690b0a278316b3
meethigher <meethigher@qq.com>
2023-07-16 19:18:56 +0800

    feat: 根据java-doc规范完善注释

--- servlet-web-framework/src/main/java/top/meethigher/webframework/utils/ServletWebUtils.java
+++ servlet-web-framework/src/main/java/top/meethigher/webframework/utils/ServletWebUtils.java
@@ -27,6 +27,9 @@ public class ServletWebUtils {
      * 1. value为null的字段，自动省略
      * 2. key值默认不带引号
      * 写法过于麻烦，所以提出一个方法
+     *
+     * @param o 对象
+     * @return 将o转换后的json字符串
      */
     public static String toJSONString(Object o) {
         return JSON.toJSONString(o, SerializerFeature.WRITE_MAP_NULL_FEATURES, SerializerFeature.QuoteFieldNames);
@@ -53,8 +56,11 @@ public class ServletWebUtils {
     }
 
     /**
-     * 这边建议使用Map<String,Object>，本身流反序列化后，就是有类型的。
+     * 这边建议使用Map&lt;String,Object&gt;，本身流反序列化后，就是有类型的。
      * 如果传过来的value是Long、Integer、Boolean等类型，强转String会报错
+     *
+     * @param url 带参url。如/interface?name=1&age=2
+     * @return 参数Map
      */
     public static Map<String, Object> getParameters(String url) {
         if (url == null || (url = url.trim()).length() == 0) {
@@ -85,6 +91,9 @@ public class ServletWebUtils {
 
     /**
      * 流转字符串
+     *
+     * @param inputStream 流
+     * @return 字符串
      */
     public static String convertInputStreamToString(InputStream inputStream) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
@@ -108,6 +117,7 @@ public class ServletWebUtils {
      * @param clazz class
      * @param <T>   类型
      * @return 转换后的对象
+     * @throws ServletWebException 异常
      */
     public static <T> T toObject(Map<String, Object> args, Class<T> clazz) throws ServletWebException {
         T t = JSON.toJavaObject((JSON) JSON.toJSON(args), clazz);
@@ -115,6 +125,13 @@ public class ServletWebUtils {
         return t;
     }
 
+    /**
+     * 校验必填
+     *
+     * @param t   对象
+     * @param <T> 对象类型
+     * @throws ServletWebException 校验不通过丢异常
+     */
     private static <T> void checkRequired(T t) throws ServletWebException {
         List<Field> requiredFields = new ArrayList<>();
         Class<?> clazz = t.getClass();
--- servlet-web-framework/src/main/java/top/meethigher/webframework/worker/ControllerMethodCaller.java
+++ servlet-web-framework/src/main/java/top/meethigher/webframework/worker/ControllerMethodCaller.java
@@ -10,12 +10,14 @@ import java.util.Map;
  */
 public interface ControllerMethodCaller {
 
+
     /**
      * 执行方法逻辑
      * 为何这边使用一个Map接? 而不是一个泛型对象。考虑GET时的请求参数，参数较少，完全不需要定义一个Model来接，所以用Map会方便一点
      *
      * @param map 参数
-     * @throws Exception 异常
+     * @return 执行结果
+     * @throws Exception 执行异常
      */
     Object exec(Map<String, Object> map) throws Exception;
 }
